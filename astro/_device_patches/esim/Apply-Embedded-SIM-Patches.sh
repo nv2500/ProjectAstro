5e1ec1176f97fe3bea08433a4060f016bab31be2
Nhat Vo <nhat.vo@dreampayments.com>
2026-01-30 22:26:32 -0700

    force remove eSIM on r9s build

--- astro/_device_patches/esim/Apply-Embedded-SIM-Patches.sh
+++ astro/_device_patches/esim/Apply-Embedded-SIM-Patches.sh
@@ -17,10 +17,10 @@
 
 
 
-if GET_FEATURE SOURCE_HAVE_ESIM_SUPPORT; then
-    if GET_FEATURE DEVICE_HAVE_ESIM_SUPPORT; then
-        LOG_END "No eSIM changes required"
-    else
+# if GET_FEATURE SOURCE_HAVE_ESIM_SUPPORT; then
+#     if GET_FEATURE DEVICE_HAVE_ESIM_SUPPORT; then
+#         LOG_END "No eSIM changes required"
+#     else
         LOG_BEGIN "Device does NOT support eSIM, removing blobs"
             NUKE_BLOAT "EsimKeyString" "EuiccService"
 
@@ -31,24 +31,24 @@ if GET_FEATURE SOURCE_HAVE_ESIM_SUPPORT; then
 
             FF "COMMON_CONFIG_EMBEDDED_SIM_SLOTSWITCH" ""
         LOG_END "eSIM blobs removed"
-    fi
-else
-    if GET_FEATURE DEVICE_HAVE_ESIM_SUPPORT; then
-        LOG_BEGIN "Device supports eSIM, adding blobs"
+#     fi
+# else
+#     if GET_FEATURE DEVICE_HAVE_ESIM_SUPPORT; then
+#         LOG_BEGIN "Device supports eSIM, adding blobs"
 
-    ADD_FROM_FW "pa3q" "system" "priv-app/EsimKeyString"
-    ADD_FROM_FW "pa3q" "system" "priv-app/EuiccService"
+#     ADD_FROM_FW "pa3q" "system" "priv-app/EsimKeyString"
+#     ADD_FROM_FW "pa3q" "system" "priv-app/EuiccService"
 
-    ADD_FROM_FW "pa3q" "system" "etc/permissions/privapp-permissions-com.samsung.android.app.esimkeystring.xml"
-    ADD_FROM_FW "pa3q" "system" "etc/permissions/privapp-permissions-com.samsung.euicc.xml"
+#     ADD_FROM_FW "pa3q" "system" "etc/permissions/privapp-permissions-com.samsung.android.app.esimkeystring.xml"
+#     ADD_FROM_FW "pa3q" "system" "etc/permissions/privapp-permissions-com.samsung.euicc.xml"
 
-    ADD_FROM_FW "pa3q" "system" "etc/sysconfig/preinstalled-packages-com.samsung.android.app.esimkeystring.xml"
-    ADD_FROM_FW "pa3q" "system" "etc/sysconfig/preinstalled-packages-com.samsung.euicc.xml"
+#     ADD_FROM_FW "pa3q" "system" "etc/sysconfig/preinstalled-packages-com.samsung.android.app.esimkeystring.xml"
+#     ADD_FROM_FW "pa3q" "system" "etc/sysconfig/preinstalled-packages-com.samsung.euicc.xml"
 
-    FF_IF_DIFF "stock" "COMMON_CONFIG_EMBEDDED_SIM_SLOTSWITCH"
+#     FF_IF_DIFF "stock" "COMMON_CONFIG_EMBEDDED_SIM_SLOTSWITCH"
 
-        LOG_END "eSIM blobs added"
-    else
-        LOG_END "No eSIM changes required"
-    fi
-fi
+#         LOG_END "eSIM blobs added"
+#     else
+#         LOG_END "No eSIM changes required"
+#     fi
+# fi
