a7782532ea1cd425d1ed4bb9d8b2d7173ea0c04d
Nhat Vo <nhat.vo@dreampayments.com>
2026-01-31 22:49:31 -0700

    v2.0.8-003

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ export BASH_WARN_ON_NULL=0
 ASTROROM="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export ASTROROM
 
-ROM_VERSION="2.0.8-002"
+ROM_VERSION="2.0.8-003"
 
 BETA_ASSERT=0
 BETA_OTA_URL=""
